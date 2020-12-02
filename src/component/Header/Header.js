8e8ca43ad7d82ebdd1b78994eb500b51870e1f9d
awuuu0716 <llinn927716@gmail.com>
2020-12-02 22:50:31 +0800

    hover nav compelete

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import logo from '../../img/logo.svg';
@@ -14,9 +15,10 @@ const HeaderContainer = styled.div`
 `;
 
 const LinkContainer = styled.div`
-  width: 400px;
+  width: ${(props) => props.$width}px;
   display: flex;
   justify-content: space-between;
+  margin-left: ${(props) => props.$marginLeft}px;
 `;
 
 const IconContainer = styled.div`
@@ -41,7 +43,6 @@ const Logo = styled(Link)`
   height: 55px;
   background: url(${logo}) no-repeat;
   cursor: pointer;
-
 `;
 
 const IconCart = styled.div`
@@ -69,19 +70,55 @@ const IconMember = styled.div`
   }
 `;
 
+const HoverContainer = styled.div`
+  display: ${({ $isShow }) => ($isShow ? 'flex' : 'none')};
+  position: absolute;
+  width: 100%;
+  top: 80px;
+  height: 80px;
+  align-items: center;
+  justify-content: center;
+  padding: 0 50px;
+  background-color: rgba(7, 39, 60, 0.4);
+  z-index: 1;
+  transition: top 0.2s ease-in-out;
+`;
+
 export default function Header() {
+  const [isShowProducts, setIsShowProducts] = useState(false);
+
+  const handleMouseEnter = () => {
+    if (isShowProducts) return;
+    setIsShowProducts(true);
+  };
+
+  const handleMouseLeave = () => setIsShowProducts(false);
+
   return (
-    <HeaderContainer>
-      <Logo to="/" />
-      <LinkContainer>
-        <Nav to="/about">關於我們</Nav>
-        <Nav to="/news">最新消息</Nav>
-        <Nav to="products">選購商品</Nav>
-      </LinkContainer>
-      <IconContainer>
-        <IconCart></IconCart>
-        <IconMember></IconMember>
-      </IconContainer>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Logo to="/" />
+        <LinkContainer $width={400}>
+          <Nav to="/about">關於我們</Nav>
+          <Nav to="/news">最新消息</Nav>
+          <Nav to="products" onMouseEnter={handleMouseEnter}>
+            選購商品
+          </Nav>
+        </LinkContainer>
+        <IconContainer>
+          <IconCart></IconCart>
+          <IconMember></IconMember>
+        </IconContainer>
+      </HeaderContainer>
+
+      <HoverContainer $isShow={isShowProducts} onMouseLeave={handleMouseLeave}>
+        <LinkContainer $width={550} $marginLeft={110}>
+          <Nav to="/about">音響</Nav>
+          <Nav to="/news">入耳式耳機</Nav>
+          <Nav to="products">耳罩式耳機</Nav>
+          <Nav to="products">週邊配件</Nav>
+        </LinkContainer>
+      </HoverContainer>
+    </>
   );
 }
