72086b24cdd1810fb92da51c41695384c6868da1
awuuu0716 <llinn927716@gmail.com>
2020-12-18 19:41:01 +0800

    add login signup

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: calc(100vh - 477px);
+`;
+
+const InputSection = styled.section`
+  display: flex;
+  width: 500px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+  padding: 60px;
+  box-shadow: 3px 3px 5px #b9e1fb;
+
+  border: 1px solid #1c79b7;
+
+  h5 {
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+`;
+
+const InputContainer = styled.div`
+  width: 320px;
+  margin: 20px 0;
+`;
+
+const OptionsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 20px;
+  justify-content: space-around;
+`;
+
+const SignUpLink = styled(Link)`
+  display: flex;
+  margin-right: 10px;
+  color: #1c79b7;
+  font-size: 16px;
+  font-weight: bold;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: none;
+    transform: scale(1.2);
+  }
+`;
+
+const LoginButton = styled.button`
+  width: 140px;
+  height: 40px;
+  display: flex;
+  border: 1px solid #07273c;
+  border-radius: 3px;
+  margin-right: 10px;
+  color: white;
+  background: #07273c;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 3px 3px 5px #07273c85;
+
+  &:hover {
+    background: #293e4c;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  font-size: 22px;
+`;
+const Input = styled.input`
+  width: 220px;
+  border: 0;
+  border-bottom: 1px solid #333;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 20px;
+  font-weight: bold;
+  height: 36px;
+`;
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Container>
+      <InputSection>
+        <h5>登入</h5>
+        <InputContainer>
+          <Label>帳號：</Label>
+          <Input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          ></Input>
+        </InputContainer>
+        <InputContainer>
+          <Label>密碼：</Label>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Input>
+        </InputContainer>
+        <ErrorMessage>帳號或密碼錯誤</ErrorMessage>
+        <OptionsContainer>
+          <SignUpLink to="/signup">建立帳戶</SignUpLink>
+          <LoginButton>登入</LoginButton>
+        </OptionsContainer>
+      </InputSection>
+    </Container>
+  );
+}
